e476c6305ead8937f4da5895f3d5dea6a5de94bc
Keyvan Mosharraf <github@keyvanm.com>
2022-04-19 23:01:05 -0700

    Draft workings

--- django_io/models/abstracts.py
+++ django_io/models/abstracts.py
@@ -1,34 +1,19 @@
 import traceback
 
-from django.core.files import File
 from django.db import models, transaction
+from django.db.models.base import ModelBase
 from django.utils.timezone import now
 from django_fsm import FSMIntegerField, transition
 
 from .enums import *
+from .mixins import *
 
 
-class DataImporter:
-    file: File
-    _clean_errors = dict
-
-    def _file_to_unprocessed_data(self):
-        raise NotImplementedError
-
-    def full_clean(self, unprocessed_data):
-        return unprocessed_data
-
-    @property
-    def clean_errors(self):
-        if self._clean_errors is None:
-            self.full_clean(self._file_to_unprocessed_data())
-        return self._clean_errors
-
-    def is_valid(self):
-        return not self.clean_errors
-
-
-class AbstractImportJob(models.Model):
+class AbstractImportJob(
+    RawDataValidatorMixin,
+    DataProcessorMixin,
+    models.Model
+):
     class Meta:
         abstract = True
 
@@ -40,22 +25,45 @@ class AbstractImportJob(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     started_on = models.DateTimeField(null=True, blank=True)
     completed_on = models.DateTimeField(null=True, blank=True)
+    last_status_update = models.DateTimeField(null=True, blank=True)
     status = FSMIntegerField(choices=ImportJobStatus.choices)
 
-    errors = models.TextField(blank=True)
+    data = models.JSONField(null=True, blank=True)
 
-    @transition(status, source=(ImportJobStatus.not_started, ImportJobStatus.errors), target=ImportJobStatus.started)
+    errors = models.JSONField(null=True, blank=True)
+    exceptions = models.TextField(blank=True)
+
+    @transition(
+        status,
+        source=(ImportJobStatus.not_started,
+                ImportJobStatus.file_processed,
+                ImportJobStatus.file_errors,
+                ImportJobStatus.errors),
+        target=ImportJobStatus.started
+    )
     def start(self):
-        self.started_on = now()
+        self.last_status_update = self.started_on = now()
 
-    @transition(status, source=ImportJobStatus.started, target=ImportJobStatus.success)
-    def complete_successfully(self):
-        self.completed_on = now()
+    @transition(status, source=ImportJobStatus.started, target=ImportJobStatus.file_processed)
+    def mark_file_processed(self):
+        self.last_status_update = now()
 
-    @transition(status, source=ImportJobStatus.started, target=ImportJobStatus.errors)
-    def complete_w_errors(self, errors):
-        self.completed_on = now()
+    @transition(status, source=ImportJobStatus.started, target=ImportJobStatus.file_errors)
+    def mark_file_errors(self, errors):
+        self.last_status_update = now()
         self.errors = errors
+
+    @transition(status, source=ImportJobStatus.file_processed, target=ImportJobStatus.success)
+    def mark_success(self):
+        self.last_status_update = self.completed_on = now()
+
+    @transition(status, target=ImportJobStatus.errors)
+    def mark_errors(self, formatted_stacktrace):
+        self.last_status_update = now()
+        self.exceptions = formatted_stacktrace
+
+    def is_valid(self):
+        pass
 
     def _process_file(self):
         raise NotImplementedError
@@ -69,7 +77,6 @@ class AbstractImportJob(models.Model):
             self.save()
         try:
             data = self._process_file()
-            self._process_data(data)
         except:
             with transaction.atomic():
                 self.complete_w_errors(traceback.format_exc())
@@ -87,3 +94,43 @@ class AbstractImportJob(models.Model):
 
         if self.auto_start and self.status == ImportJobStatus.not_started:
             self.process()
+
+
+class SheetImporterMeta(ModelBase):
+    """Collect Fields declared on the base classes."""
+
+    @property
+    def _label_to_keys_map(cls):
+        return {
+            field.label or key: key
+            for key, field in cls.form_class.base_fields.items()
+        }
+
+    @property
+    def _keys_to_labels_map(cls):
+        return {
+            key: field.label or key
+            for key, field in cls.form_class.base_fields.items()
+        }
+
+
+class AbstractSheetImportJob(AbstractImportJob, metaclass=SheetImporterMeta):
+    class Meta:
+        abstract = True
+
+    form_class = None
+
+    def _process_file(self):
+        pass
+
+    def _process_data(self, data):
+        pass
+
+    def get_instances(self):
+        return [form.get_instance() for form in self._forms]
+
+    def save_data(self):
+        if self.errors:
+            raise ValueError("Import could not be run because the data didn't validate")
+
+        self.model.objects.bulk_update_or_create(self.get_instances())
--- django_io/models/enums.py
+++ django_io/models/enums.py
@@ -4,6 +4,8 @@ from django.db import models
 class ImportJobStatus(models.IntegerChoices):
     not_started = 0
     started = 20
+    file_processed = 40
+    file_errors = 41
     success = 100
     errors = 101
 
